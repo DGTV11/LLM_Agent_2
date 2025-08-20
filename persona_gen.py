1ed0b78a7b8dc1ae959c8d6072f3da71e2a80e3d
Daniel <52272655+DGTV11@users.noreply.github.com>
2025-08-21 07:12:01 +0800

    Update persona_gen.py

--- persona_gen.py
+++ persona_gen.py
@@ -1,6 +1,8 @@
 from pocketflow import *
 import yaml
 from prompts import PERSONA_GEN_PROMPT
+from config import PERSONA_MAX_WORDS
+from llm import call_llm
 
 class GeneratePersona(Node):
     def prep(self, shared):
@@ -8,4 +10,7 @@ class GeneratePersona(Node):
         return goals
         
     def exec(self, goals):
+        prompt = PERSONA_GEN_PROMPT.format(goals, PERSONA_MAX_WORDS)
+        
+        res = call_llm()
         pass
