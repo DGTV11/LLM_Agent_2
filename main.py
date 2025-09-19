7a299414d78bda9cd20702e66ca82bd2c4ced670
Daniel Wee <danielweedk@gmail.com>
2025-09-19 10:17:51 +0800

    fixed potential bug

--- main.py
+++ main.py
@@ -339,12 +339,13 @@ async def chat(agent_id: str, websocket: WebSocket):
                 agent_id,
                 UserOrSystemMessage(
                     message_type="system",
-                    message="The user has exited the conversation. You should do some background tasks (if necessary) before going into standby mode.",
+                    # message="The user has exited the conversation. You should do some background tasks (if necessary) before going into standby mode.",
+                    message="The user has exited the conversation.",
                 ),
             )
 
-            for _ in agent.call_agent(agent_id):
-                await asyncio.sleep(0.05)
+            # for _ in agent.call_agent(agent_id):
+            #     await asyncio.sleep(0.05)
 
             scheduler.add_job(
                 heartbeat_query,
